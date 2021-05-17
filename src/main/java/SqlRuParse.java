2fa8d035684fdaf10255c6cc59627453fafdfe8c
Usov Aleksei <usalekse@gmail.com>
2021-05-17 15:58:10 +0300

    7.6.1.2. Парсинг HTML страницы. [#406004]

---
+++ src/main/java/SqlRuParse.java
@@ -0,0 +1,20 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class SqlRuParse {
+
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            System.out.println(td.parent().child(5).text());
+        }
+    }
+}
