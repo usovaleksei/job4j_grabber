5c50aa055836365f6e129d85e5e09049bf34b546
Usov Aleksei <usalekse@gmail.com>
2021-05-19 12:39:39 +0300

    7.6.2.3. Загрузка деталей поста. [#406010]

--- src/main/java/SqlRuParse.java
+++ src/main/java/SqlRuParse.java
@@ -4,8 +4,23 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class SqlRuParse {
+
+    private void getVacancyInfo(String href) throws IOException {
+        Document doc = Jsoup.connect(href).get();
+        Element postDescription = doc.getElementsByClass("msgBody").get(1);
+        String description = postDescription.text();
+        Element date = doc.getElementsByClass("msgFooter").get(0);
+        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+        String parseDate = date.text().split("\\[")[0].trim();
+        LocalDateTime createdDate = parser.parse(parseDate);
+        System.out.println("Описание вакансии: ");
+        System.out.println(description);
+        System.out.println("Дата создания вакансии: ");
+        System.out.println(createdDate);
+    }
 
     public static void main(String[] args) throws IOException {
         for (int i = 1; i < 6; i++) {
