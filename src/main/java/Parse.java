5823bad8c8c87df3aa6d7239cc7e5f3bac5b7e08
Usov Aleksei <usalekse@gmail.com>
2021-05-21 00:45:30 +0300

    7.6.5.1. Refactor files by Grabber project

--- src/main/java/Parse.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.util.List;
-
-public interface Parse {
-
-    List<Post> list(String link) throws IOException;
-
-    Post detail(String link) throws IOException;
-}
